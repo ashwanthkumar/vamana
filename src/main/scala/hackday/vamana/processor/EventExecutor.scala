28080467a9bb4db2b079203cc6ae76003613ac5f
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2015-05-23 15:37:33 +0530

    we're not getting provisioner for everything

--- src/main/scala/hackday/vamana/processor/EventExecutor.scala
+++ src/main/scala/hackday/vamana/processor/EventExecutor.scala
@@ -1,16 +1,17 @@
 package hackday.vamana.processor
 
 import hackday.vamana.models.{Cluster, Event, ClusterStore}
-import hackday.vamana.provisioner.Provisioner
+import hackday.vamana.provisioner.{ClusterProvisioner, Provisioner}
 
-class EventExecutor(event: Event, store: ClusterStore, provisioner: Provisioner) extends Runnable {
+class EventExecutor(event: Event, store: ClusterStore) extends Runnable {
   import hackday.vamana.models.Events._
   override def run(): Unit = {
     println(s"I'm executing $event")
     event match {
       case Create(spec) =>
         val uniqueClusterId = store.nextId
-        provisioner.create(Cluster.fromSpec(spec ++ Map("id" -> uniqueClusterId.toString)))
+        val cluster = Cluster.fromSpec(spec ++ Map("id" -> uniqueClusterId.toString))
+        ClusterProvisioner.create(cluster)
     }
   }
 }
