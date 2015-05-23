3d6273f189853b5a950cea9b54bc3b5cc0ab9472
Sriram <sriram@indix.com>
2015-05-23 20:40:51 +0530

    WIP - Implementation for MyServiceScalar complete. Collector in progress

--- src/main/scala/hackday/vamana/scalar/myservice/MyServiceCollector.scala
+++ src/main/scala/hackday/vamana/scalar/myservice/MyServiceCollector.scala
@@ -4,5 +4,6 @@ import hackday.vamana.scalar.{ResourceStat, Collector}
 import hackday.vamana.models.RunningCluster
 
 class MyServiceCollector(cluster: RunningCluster) extends Collector {
+  
   override def getStats: ResourceStat = ???
 }
--- src/main/scala/hackday/vamana/scalar/myservice/MyServiceScalar.scala
+++ src/main/scala/hackday/vamana/scalar/myservice/MyServiceScalar.scala
@@ -2,11 +2,15 @@ package hackday.vamana.scalar.myservice
 
 import hackday.vamana.scalar.{ScaleUnit, ResourceStat, Scalar}
 import hackday.vamana.models.RunningCluster
+import hackday.vamana.util.VamanaLogger
 
-class MyServiceScalar(cluster: RunningCluster) extends Scalar(cluster) {
+class MyServiceScalar(cluster: RunningCluster) extends Scalar(cluster) with VamanaLogger {
   override def scaleUnit(normalizedStat: ResourceStat): ScaleUnit = ScaleUnit(normalizedStat.demand.quantity - normalizedStat.supply.quantity)
 
   override def downscaleCandidates(number: Int): List[String] = {
-
+    cluster.context match {
+      case Some(ctx) => cluster.context.fold(List[String]())(_.slaves.take(number).map(_.getId).toList)
+      case None => LOG.warn(s"No cluster context available for this cluster: ${cluster.spec.name}. Nothing to downscale."); List[String]()
+    }
   }
 }
