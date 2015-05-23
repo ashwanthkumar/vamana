fc9ba24fe820a634c595cfa50b51ab5989606838
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2015-05-23 21:03:01 +0530

    Adding logs around how long each operation took

--- src/main/scala/hackday/vamana/processor/EventExecutor.scala
+++ src/main/scala/hackday/vamana/processor/EventExecutor.scala
@@ -3,6 +3,7 @@ package hackday.vamana.processor
 import hackday.vamana.models._
 import hackday.vamana.provisioner.ClusterProvisioner
 import hackday.vamana.util.VamanaLogger
+import org.apache.commons.lang.time.{DurationFormatUtils, StopWatch}
 
 class EventExecutor(event: Event, store: ClusterStore) extends Runnable with VamanaLogger {
 
@@ -11,6 +12,8 @@ class EventExecutor(event: Event, store: ClusterStore) extends Runnable with Vam
   override def run(): Unit = {
     event match {
       case Create(spec, uniqueClusterId) =>
+        val watch = new StopWatch
+        watch.start()
         LOG.info(s"Starting to create cluster with ${spec.mkString(",")}")
         val clusterSpec = ClusterSpec.fromSpec(spec)
         val initializingCluster = RunningCluster(uniqueClusterId, clusterSpec, Booting, None)
@@ -22,25 +25,31 @@ class EventExecutor(event: Event, store: ClusterStore) extends Runnable with Vam
         LOG.info(s"Master - ${clusterContext.master.getPublicAddresses} / ${clusterContext.master.getPrivateAddresses}")
         clusterContext.slaves.foreach(slave => LOG.info(s"Started slave - ${slave.getPublicAddresses} / ${slave.getPrivateAddresses}"))
         store.save(runningCluster)
+        LOG.info(s"Cluster ${clusterSpec.name} has been created in ${DurationFormatUtils.formatDurationHMS(watch.getTime)}")
 
       case Upscale(clusterId, factor) =>
+        val watch = new StopWatch
+        watch.start()
         val clusterOption = store.get(clusterId)
         for (
           cluster <- clusterOption;
           context <- cluster.context
         ) {
           ClusterProvisioner.upScale(cluster.spec, context, factor)
-          LOG.info(s"Upscaled the ${cluster.spec.name} by $factor nodes")
+          watch.stop()
+          LOG.info(s"Upscaled the ${cluster.spec.name} by $factor nodes in ${DurationFormatUtils.formatDurationHMS(watch.getTime)}")
         }
 
       case Downscale(clusterId, factor) =>
+        val watch = new StopWatch
+        watch.start()
         val clusterOption = store.get(clusterId)
         for (
           cluster <- clusterOption;
           context <- cluster.context
         ) {
           ClusterProvisioner.downScale(cluster.spec, context, factor)
-          LOG.info(s"Downscaled the ${cluster.spec.name} by $factor nodes")
+          LOG.info(s"Downscaled the ${cluster.spec.name} by $factor nodes in ${DurationFormatUtils.formatDurationHMS(watch.getTime)}")
         }
     }
   }
