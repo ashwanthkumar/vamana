2452b3f9343e333f10c25c4bfc2f325e2fcd0360
Sriram <sriram@indix.com>
2015-05-24 05:36:51 +0530

    Enabling foobar service

--- src/main/scala/hackday/vamana/models/AppLifeCycle.scala
+++ src/main/scala/hackday/vamana/models/AppLifeCycle.scala
@@ -60,7 +60,7 @@ case class MyServiceLifeCycle(clusterContext: ClusterContext) extends AppLifeCyc
       Copy(mySvcPath, remoteMySVCFullPath)
     )
     val cmd = List(
-      "python -m SimpleHTTPServer 8080 &"
+      "python /home/ec2-user/MyService.py 8080 &"
     )
 
     Bootstrap(copyActions, cmd)
@@ -68,7 +68,7 @@ case class MyServiceLifeCycle(clusterContext: ClusterContext) extends AppLifeCyc
 
   override def decommission(): Unit = {
     Decommission(
-      List("ps -aef | grep SimpleHTTPServer | grep -v | awk '{print $2}' | xargs kill -9")
+      List("ps -aef | grep MyService | grep -v | awk '{print $2}' | xargs kill -9")
     )
   }
 }
